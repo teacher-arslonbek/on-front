506d3940c9da4aa8196fd263228e0ecf53cce069
Arslonbek Alimbayev <arslonbekalimbayev@Arslonbeks-MacBook-Pro.local>
2021-06-25 04:57:43 +0500

    lesson-8 start-code

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const numbers = [1, 20, 3, 4, 5];
+/**
+ * Joining arrays
+ */
+const joined = numbers.join("--");
+console.log(`joined = ${numbers}`);
+console.log(`joined = ${joined}`);
