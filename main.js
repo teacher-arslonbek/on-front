c655425dbd5df01f5bce6282793a0f45ed6d32dc
arslonbekXX <arslonbek.lion067@gmail.com>
2021-08-19 21:39:20 +0500

    lesson-20 finish-code

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Welcome To Lesson-20");
+const fetch = require("node-fetch");
+let users = [];
+const getUsers = async () => {
+  const response = await fetch("http://jsonplaceholder.typicode.com/users");
+  setTimeout(() => console.log("Hello World"), 10000);
+  users = await response.json();
+};
+
+const deleteUser = (selectedUserID) => {
+  users = users.filter((user) => user.id !== selectedUserID);
+};
+
+async function start() {
+  await getUsers();
+  console.log("old users = ", users);
+  deleteUser(1);
+  console.log("lastUsers users = ", users);
+}
+
+start();
+
+async function first() {}
+function second() {}
